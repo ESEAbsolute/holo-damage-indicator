9825bae7067ddf80af64a0af28a9fe82fd7beb4c
Ray <221049@hchs.edu.my>
2026-05-31 00:57:55 +0800

    add 1.21.11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ stonecutter {
 		fun match(version: String, vararg loaders: String) =
 			loaders.forEach { version("$version-$it", version).buildscript = getBuildscript(it, version) }
 		match("26.1", "fabric")//TODO:add neoforge support(
-//		match("1.21.11", "fabric","neoforge")
+		match("1.21.11", "fabric")
 		match("1.20.1","fabric","forge")
 		match("1.21.1", "fabric","neoforge")
 
